f9de7a3317731129e952f37ca453e5a65c102138
2544403004@qq.com <2544403004@qq.com>
2017-03-11 22:37:02 +0800

    命令模式

---
+++ 命令模式/menu/index.js
@@ -0,0 +1,52 @@
+var button1=document.querySelector('#button1'),
+button2=document.querySelector('#button2'),
+button3=document.querySelector('#button3');
+
+
+var setCommand=function(button,command){
+	button.onclick=function(){
+		command.execute();
+	}
+}
+
+var MenuBar={
+	refresh:function(){
+		console.log('刷新菜单目录')
+	}
+}
+
+var SubMenu={
+	add:function(){
+		console.log('增加子菜单')
+	},
+	del:function(){
+		console.log('删除子菜单')
+	}
+}
+
+var RefreshMenuBarCommand=function(receiver){
+	this.receiver=receiver;
+}
+
+RefreshMenuBarCommand.prototype.execute=function(){
+	this.receiver.refresh();
+}
+var AddSubMenuCommand=function(receiver){
+	this.receiver=receiver;
+}
+AddSubMenuCommand.prototype.execute=function(){
+	this.receiver.add();
+}
+var DelSubMenuCommand=function(receiver){
+	this.receiver=receiver;
+}
+DelSubMenuCommand.prototype.execute=function(){
+	console.log('删除子菜单');
+}
+var refreshMenuBarCommand=new RefreshMenuBarCommand(MenuBar);
+var addSubMenuCommand=new AddSubMenuCommand(SubMenu);
+var delSubMenuCommand=new DelSubMenuCommand(SubMenu);
+
+setCommand(button1,refreshMenuBarCommand);
+setCommand(button2,addSubMenuCommand);
+setCommand(button3,delSubMenuCommand);
